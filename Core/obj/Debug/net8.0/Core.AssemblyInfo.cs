ec25586d040c1e5e8c263ae2edf8565c4187e7f2
Santiago Chaverra López <saintgogo6@gmail.com>
2025-04-18 00:45:36 -0500

    .yml fixes

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a70943ec24278e792d148d8b76d3a543cbdb1f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6171f93bfd0fefad310be2a901670369d3e614d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
