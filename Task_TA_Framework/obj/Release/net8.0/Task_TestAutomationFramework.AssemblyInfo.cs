c8c3b9ef67a22b2ddb24f4aeabb8a55d03fd8d6b
Santiago Chaverra López <saintgogo6@gmail.com>
2025-04-21 22:57:54 -0500

    build fixes 10

--- Task_TA_Framework/obj/Release/net8.0/Task_TestAutomationFramework.AssemblyInfo.cs
+++ Task_TA_Framework/obj/Release/net8.0/Task_TestAutomationFramework.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_TestAutomationFramework")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6171f93bfd0fefad310be2a901670369d3e614d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ce808ceb2431b82e9b156d8c8a708b39ab9b52")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_TestAutomationFramework")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task_TestAutomationFramework")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
